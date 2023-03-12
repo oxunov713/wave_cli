52935a077c5b23a05a4cdf13c336f580b351d219
Your Name <you@example.com>
2023-03-12 15:39:28 +0500

    first commit

---
+++ bin/wave_cli.dart
@@ -0,0 +1,6 @@
+import 'package:wave_sdk/wave_sdk.dart';
+import 'package:wave_sla/wave_cli.dart';
+
+void main(List<String> arguments) {
+  print(Calculator.summ([34, 65, 787]));
+}
---
+++ test/wave_cli_test.dart
@@ -0,0 +1,9 @@
+
+import 'package:test/test.dart';
+import 'package:wave_sla/wave_cli.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
